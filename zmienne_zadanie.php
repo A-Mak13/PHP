1a3e029ac6ec2bcb03e2bcbeb47e2b26d29aee6d
A-Mak13 <angelikamaktia@gmail.com>
2025-09-04 15:17:53 +0200

    ćwiczenia na lekcji

--- zmienne_zadanie.php
+++ zmienne_zadanie.php
@@ -3,6 +3,6 @@ $a_bool= true; //boolean
 $a_str="foo"; //string
 $an_int= 12; //integer
 $pi_float= 3.14; //float
-echo gettype($a_bool);
-echo gettype($a_str);
+echo gettype($a_bool) . "<br>"; //wypisze boolean
+echo gettype($a_str); //wypisze string
 ?>
