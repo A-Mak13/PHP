fcfef9238db879b6120b8fac7b2505155b88929f
A-Mak13 <angelikamaktia@gmail.com>
2025-09-04 15:31:55 +0200

    ćwiczenia na lekcji

---
+++ String.php
@@ -0,0 +1,16 @@
+<?php
+echo 'this is a simple string';
+
+echo 'You can also have embedded newlines in
+strings this way as it is
+okay to do';
+
+//"I'll be back"
+echo 'Arnold once said: "I\'ll be back"';
+
+//You deleted C:\*.*?
+echo 'You deleted C:\\*.*?';
+
+//You deleted C:\*.*?
+echo 'You deleted C:\*.*?';
+?>
