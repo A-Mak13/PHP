58c72f59db255ed9e1322ea97a4f9ee9415dfa53
A-Mak13 <angelikamaktia@gmail.com>
2025-09-08 10:44:27 +0200

    zadanie

--- printf.php
+++ printf.php
@@ -1,7 +1,8 @@
 <?php
     $tekst= "hello";
-    printf("[%s]\n, $tekst"); //wypisze [hello]
-    printf("[%10s]\n, $tekst"); //wypisze [     hello]
-    printf("[%-10s]\n, $tekst"); //wypisze [hello   ]
-    printf("[%.3s]\n, $tekst"); //wypisze [hel]
-    printf("[%10.3s]\n, $tekst"); //wypisze [   hel]
+    printf("[%s]\n", $tekst); //wypisze [hello]
+    printf("[%10s]\n", $tekst); //wypisze [     hello]
+    printf("[%-10s]\n", $tekst); //wypisze [hello   ]
+    printf("[%.3s]\n", $tekst); //wypisze [hel]
+    printf("[%10.3s]\n", $tekst); //wypisze [   hel]
+?>
