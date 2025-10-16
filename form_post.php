906805c9cda9e1d04d3d456c37cebe203255741b
A-Mak13 <angelikamaktia@gmail.com>
2025-10-16 15:41:49 +0200

    zadania php

--- form_post.php
+++ form_post.php
@@ -1,4 +1,4 @@
-Welcome <?php echo $_POST["name"], $_POST["nazwisko"]; ?> <br>
+Welcome <?php echo $_POST["name"]. " ".  $_POST["nazwisko"]; ?> <br>
 Your email address is: <?php echo $_POST["email"]; ?>
 <?php
 $servername = "localhost";
@@ -10,7 +10,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-
 $name = ($_POST['name']);
 $lastname =($_POST['nazwisko']);
 $email =($_POST['email']);
@@ -18,14 +17,11 @@ $email =($_POST['email']);
 
 $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('$name', '$lastname', '$email')";
 
-
 if (mysqli_query($conn, $sql)) {
     echo "<br>" . "New record created successfully<br>";
     
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
-
 mysqli_close($conn);
 ?>
